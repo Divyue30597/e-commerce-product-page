11dfb99628520943630053190b0a77e99430e671
Divyue30597 <tomchi.sharma305@outlook.com>
2023-05-06 11:52:26 +0530

    done with image gallery

--- src/ProductImage/ProductImage.tsx
+++ src/ProductImage/ProductImage.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import productImg1Thumb from "../assets/images/image-product-1-thumbnail.jpg";
+import productImg1 from "../assets/images/image-product-1.jpg";
+import productImg2Thumb from "../assets/images/image-product-2-thumbnail.jpg";
+import productImg2 from "../assets/images/image-product-2.jpg";
+import productImg3Thumb from "../assets/images/image-product-3-thumbnail.jpg";
+import productImg3 from "../assets/images/image-product-3.jpg";
+import productImg4Thumb from "../assets/images/image-product-4-thumbnail.jpg";
+import productImg4 from "../assets/images/image-product-4.jpg";
+import "./productimage.css";
+
+const productImage = [productImg1, productImg2, productImg3, productImg4];
+const productImageThumb = [
+  productImg1Thumb,
+  productImg2Thumb,
+  productImg3Thumb,
+  productImg4Thumb,
+];
+
 export function ProductImage() {
+  const [activeState, setActiveState] = useState(0);
+
   return (
-    <>
-      <h1>Product Image</h1>
-    </>
+    <section className="product-image-section">
+      <div className="product-images">
+        <picture>
+          <img src={productImage[activeState]} alt="" />
+        </picture>
+      </div>
+      <div className="thumbnail-images">
+        {productImageThumb.map((thumbImg, index) => {
+          return (
+            <picture>
+              <img
+                key={index}
+                src={thumbImg}
+                alt="product thumbnail"
+                onClick={() => setActiveState(index)}
+              />
+            </picture>
+          );
+        })}
+      </div>
+    </section>
   );
 }
